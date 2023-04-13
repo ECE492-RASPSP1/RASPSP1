01689724c10aede644596b7bf5bc67780a350a94
Kin Jiengphattanakit <84873513+Zyllys@users.noreply.github.com>
2023-04-13 01:06:33 -0600

    send command to arduino

--- software/inverse_kinematic.py
+++ software/inverse_kinematic.py
@@ -5,12 +5,16 @@ import numpy as nps
 import math
 
 import matplotlib.pyplot as plt
+import serial
 
 rail_lower_limit = 0.0
-rail_upper_limit = 1.0
+rail_upper_limit = 0.8
 robot_height = 0.735
 robot_base_height = 0
 z_height = 0.751
+
+# change this to the right port
+ser = serial.Serial("/dev/cu.usbmodem211401", 115200, timeout=1)
 
 
 robot_arm_chain = ikpy.chain.Chain.from_urdf_file("raspsp1_no_rail.urdf",active_links_mask=[False,True,True,True,True,True,False,])
@@ -37,6 +41,7 @@ def move_arm(x,y,z):
 	position = robot_arm_chain.forward_kinematics(ik)
 	print("FK Position: [%.3f, %.3f, %.3f]" % (position[0][3], position[1][3], position[2][3]))
 	# print("The angles of each joints are : ", list(map(lambda r:math.degrees(r),ik.tolist())))
+	print()
 	plot(ik)
 
 
@@ -59,11 +64,15 @@ def move(x,y,z):
 	# print("FK Position: [%.3f, %.3f, %.3f]" % (position[0][3], position[1][3], position[2][3]))
 	# print("The angles of each joints are : ", list(map(lambda r:math.degrees(r),ik.tolist())))
 	plot(ik)
+	motor_steps = ik_to_steps(ik)
+	rail_steps = r * 40000
+	command = "G01 %d %d %d %d %d %d\n" % (motor_steps[0], motor_steps[1], motor_steps[2], motor_steps[3], motor_steps[4], rail_steps)
+	ser.write(command.encode("ASCII"))
 
 
 # calculate the optimal position for the rail, such that the arm can reach desired position
 def rail_sweep(x, y, z):
-	intervals = nps.linspace(rail_lower_limit,rail_upper_limit, num = 201)
+	intervals = nps.linspace(rail_lower_limit,rail_upper_limit, num = 161)
 	fk_array = []
 	for i in intervals:
 		sweep_position = [x - i, y, z]
@@ -101,10 +110,9 @@ def plot(ik):
 
 def get_position_input():
 	x, y, z = map(float, input("Enter X,Y,Z coordinate: ").split())
-	distance = math.sqrt(x * x + y * y + z * z)
-	print("Distance: ", distance)
-	if (distance > robot_height):
-		print("Warning: This position might be outside the motion range of the arm") # need to fix this
+	# print("Distance: ", distance)
+	# if (distance > robot_height):
+	# 	print("Warning: This position might be outside the motion range of the arm") # need to fix this
 
 	if (abs(x) < 0.0001):
 		x = 0.0001
@@ -116,12 +124,55 @@ def get_position_input():
 		z = 0.0001
 	return x,y,z
 
-if __name__ == "__main__":
-	get_defualt_position()
-	# move(0.1, 0, 0.6)
+def get_waypoint():
+	n = int(input("Please Specify the number of waypoint coordinates: "))
+	coordinates = []
+	system_position = [] #[rail, x, y, z]
 
-	rail_sweep(0.5, 0, 0.751)
+	for i in range(n):
+		x, y, z = get_position_input()
+		coordinates.append([x,y,z])
+		system_position.append(rail_sweep(x,y,z))
+	
+	return system_position
+
+
+def ik_to_steps(ik_rad):
+	ik_deg = nps.rad2deg(ik_rad)
+	upper_wrist = (ik_deg[5] * 420) / 90
+	lower_wrist = (ik_deg[4] * -50) / 90
+	elbow = (ik_deg[3] * 2200) / 90
+	shoulder = (ik_deg[2] * 2000) / 90
+	base = (ik_deg[1] * 4100) / 90
+
+	#rail = 200 steps / 5 mm
+
+	print("upper_wrist: ", upper_wrist)
+	print("lower_wrist: ", lower_wrist)
+	print("elbow: ", elbow)
+	print("shoulder: ", shoulder)
+	print("base", base)
+
+	return([upper_wrist,lower_wrist,elbow,shoulder,base])
+
+if __name__ == "__main__":
+	# get_defualt_position()
+	#move(0.1, 0, 0.6)
+
+	# rail_sweep(0.5, 0, 0.751)
+
+	# print(get_waypoint())
+	command = "G01 0 0 0 0 0 0\n"
+	ser.write(command.encode("ASCII"))
 	while(True):
 		x, y, z = get_position_input()
 		move(x,y,z)
+
+		# fk = rail_sweep(x, y, z)
+		# ik = robot_arm_chain.inverse_kinematics(fk[1:4],target_orientation,orientation_mode = None)
+		# plot(ik)
+
+		# rail_steps = fk[0] * 40000
+		# print("rail: ",rail_steps)
+		# ik_to_steps(ik)
 
